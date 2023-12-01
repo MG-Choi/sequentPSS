84cd91dd8c1e2d0a1fb846ded51c42faf4ef0c5b
MC1316663 <u1316663@utah.edu>
2023-12-01 12:26:04 -0700

    comment_12.1

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'sequentPSS',
-    version = '0.0.9',
+    version = '0.1.0',
     description = "algorithm for sequential parameter searching with GSA",
     url = 'https://github.com/MG-Choi/sequentPSS',
     author = 'MoongiChoi',
